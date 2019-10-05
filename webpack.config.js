198208cd9b3cf8f1979d62806b6ee7b7018cc307
Bartosz Kaminski <Bakaminski@wp.pl>
2019-10-05 17:41:13 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+var path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/js/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/main-[contenthash:4].js"
+  },
+  devServer: {
+    open: true,
+    contentBase: path.join(__dirname, "assets")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      },
+      //   { test: /\.css$/i, use: ["style-loader", "css-loader"] },
+      {
+        test: /\.(jpg|png|jpe?g|gif)$/i,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+          outputPath: "images",
+          publicPath: "../images"
+        }
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              ["@babel/preset-env", { useBuiltIns: "usage", corejs: "2.0.0" }]
+            ]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ template: "./index.html" }),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css"
+    })
+  ]
+};
